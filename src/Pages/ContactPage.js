32b35ca7225a80a7a8bd330bdad45d314c11692f
Mountaineer Ben <bentantillo@gmail.com>
2022-02-20 21:22:04 -0800

    contact page edits, linked in card added

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -7,6 +7,7 @@ import ContactCard from '../Components/ContactCards'
 import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 import EmailIcon from '@mui/icons-material/Email';
 import PushPinIcon from '@mui/icons-material/PushPin';
+import ComputerIcon from '@mui/icons-material/Computer';
 import { motion } from 'framer-motion';
 
 const ContactStyles = styled.section`
@@ -40,6 +41,7 @@ const ContactStyles = styled.section`
 const phone = <PhoneIphoneIcon />
 const email = <EmailIcon />
 const location = <PushPinIcon />
+const computer = <ComputerIcon/>
 
 function Contact() {
     return (
@@ -66,6 +68,7 @@ function Contact() {
                         <div className='contact-cards'>
                             <ContactCard title={'Phone'} icon={phone} contact1={'C: 908-268-8346'} contact2={' '} />
                             <ContactCard title={'Email'} icon={email} contact1={'P: BenTantillo@gmail.com'} contact2={' '} />
+                            <ContactCard title={'LinkedIn'} icon={computer} contact1={'https://www.linkedin.com/in/benedicttantillo/'} contact2={''} />
                             <ContactCard title={'Address'} icon={location} contact1={'PNW'} contact2={'Portland, OR '} />
                         </div>
                     </InnerLayout>
